540ea87950157514d5f797cc9573b37b99221955
Zehuan Li <zhli@jhu.edu>
2017-12-14 18:28:57 -0500

    change program structure

--- graph_util.py
+++ graph_util.py
@@ -1,12 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def draw_relation(point_dict):
-    G = nx.DiGraph()
+
+def draw_relation(point_dict: dict):
+    g = nx.DiGraph()
     for x, y in point_dict.items():
         for t in y:
-            G.add_edge(x, t)
-    nx.draw(G, with_labels=True)
+            g.add_edge(x, t)
+    nx.draw(g, with_labels=True)
     plt.show()
 
 
